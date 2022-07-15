f1721cdc4f7bde98dc41bcb13acd78b2e839afd4
Dylan James <alazyartist@gmail.com>
2022-07-15 16:54:49 -0600

    Community Links

--- flipfactory-cra/src/components/CommunityLinks.js
+++ flipfactory-cra/src/components/CommunityLinks.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { FaDiscord, FaTwitter, FaTelegramPlane } from "react-icons/fa";
 
 const CommunityLinks = () => {
 	return (
 		<div className='flex place-items-center place-content-center gap-4'>
-			<div>Discord</div>
-			<div>Telegram</div>
-			<div>Twitter</div>
+			<div className='flex gap-2 place-content-center place-items-center'>
+				<FaDiscord />
+				Discord
+			</div>
+			<div className='flex gap-2 place-content-center place-items-center'>
+				<FaTelegramPlane />
+				Telegram
+			</div>
+			<div className='flex gap-2 place-content-center place-items-center'>
+				<FaTwitter />
+				Twitter
+			</div>
 		</div>
 	);
 };
