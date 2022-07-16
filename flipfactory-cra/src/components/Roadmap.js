764ca56579bf8601c021fa7da31c4ef14e23f8a1
Dylan James <alazyartist@gmail.com>
2022-07-16 00:57:41 -0600

    More Roadmap

--- flipfactory-cra/src/components/Roadmap.js
+++ flipfactory-cra/src/components/Roadmap.js
@@ -3,29 +3,25 @@ import React from "react";
 const Roadmap = () => {
 	return (
 		<div className='p-2 flex flex-col place-content-center place-items-center'>
-			<ul>
+			<ul className=' flex flex-col'>
 				<h1 className='text-2xl font-semibold'>Roadmap Activations</h1>
-				<li className='flex gap-2'>
-					<div>10%</div>
-					<div>Initial Mint</div>
-				</li>
-				<li className='flex gap-2'>
-					<div>20%</div>
-					<div>Access To Trickedex</div>
-				</li>
-				<li className='flex gap-2'>
-					<div>30%</div>
-					<div>Flip Factory User Page</div>
-				</li>
-				<li className='flex gap-2'>
-					<div>40%</div>
-					<div>Beta Card Game</div>
-				</li>
-				<li className='flex gap-2'>
-					<div>50%</div>
-					<div>Emote AirDrop</div>
-				</li>
+				<ListItem percent={"10"} perk='Model Release' />
+				<ListItem percent={"20"} perk='Emote Shop' />
+				<ListItem percent={"30"} perk='Secondary Market' />
+				<ListItem percent={"40"} perk='Beta Card Game' />
 			</ul>
+		</div>
+	);
+};
+
+const ListItem = ({ percent, perk }) => {
+	return (
+		<div className='h-20'>
+			<li className='flex gap-2 rounded-xl bg-zinc-800 text-zinc-300 p-2'>
+				<div>{percent}%</div>
+				<div>{perk}</div>
+			</li>
+			<div className='relative z-[-1] left-[8rem] -top-10  bg-zinc-800 rounded-xl w-5 h-[5.5rem]' />
 		</div>
 	);
 };
