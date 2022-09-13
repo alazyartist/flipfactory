58a87d2909c0df9c2cde618d660dc61f720c33fe
Dylan James <alazyartist@gmail.com>
2022-09-12 21:25:33 -0600

    Otherside Land Plots

---
+++ flipfactory-cra/src/components/OthersideLandPlots.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const OthersideLandPlots = () => {
+	return (
+		<div className='h-fit w-full font-inter text-3xl text-center'>
+			<div>
+				<div>
+					<span className='font-black'>Otherside Plot</span>
+					<span className='font-lucky '>#108</span>
+				</div>
+				<img className='rounded-xl p-2 py-1 w-full' src='./Otherside108.png' />
+			</div>
+			<div>
+				<div>
+					<span className='font-black'>Otherside Plot</span>
+					<span className='font-lucky '>#64420</span>
+				</div>
+				<img
+					className='rounded-xl p-2 px-1 w-full'
+					src='./Otherside64420.png'
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default OthersideLandPlots;
