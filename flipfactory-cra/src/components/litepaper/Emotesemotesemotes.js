51ca63d49bc802945397b34076becb41d1004709
Dylan James <alazyartist@gmail.com>
2022-10-01 19:59:19 -0600

    fix emoteemoteemotes

--- flipfactory-cra/src/components/litepaper/Emotesemotesemotes.js
+++ flipfactory-cra/src/components/litepaper/Emotesemotesemotes.js
@@ -14,15 +14,6 @@ const Emotesemotesemotes = () => {
 				dance sequences and crazy high-flying tricks at the push of a button.{" "}
 				<br />
 				<br />
-				A set of movements that you can play on command to show some emotion,
-				the concept of an emote is not new. It’s been permeating video games for
-				a good while now. These began as simple movements like a head nod or a
-				friendly wave, however over the last few years we have seen AAA games
-				incorporating more interesting acrobatic movements and dances into their
-				ecosystems, allowing players to have their characters perform full-on
-				dance sequences and crazy high-flying tricks at the push of a button.
-				<br />
-				<br />
 				These movements have always been limited to a few choices and without
 				much variety. In the coming metaverses, people will want options. They
 				will want to be able to differentiate their character’s movements from
@@ -39,11 +30,6 @@ const Emotesemotesemotes = () => {
 				character’s movements from everyone else. In order to do that we will
 				need a lot of options and ways to call on them quickly.
 				<br />
-				<br />
-				Luckily, many of these virtual worlds are building functionality like
-				this into their core for people like us to build on top of, and, “if we
-				do it right” extend the functionality. This is where our first project
-				the Flip Factory comes in.
 			</div>
 		</div>
 	);
